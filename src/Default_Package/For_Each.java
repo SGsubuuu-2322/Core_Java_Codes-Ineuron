f441ff281121abb5deb2fc137dc7ada10db8e3e9
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2022-12-28 14:46:37 +0530

    Practice Codes

---
+++ src/Default_Package/For_Each.java
@@ -0,0 +1,26 @@
+package Default_Package;
+import java.util.*;
+
+public class For_Each {
+
+	public static void main(String[] args) {
+		Scanner ip = new Scanner(System.in);
+		System.out.println("This is the program for entering an array and then printing that array through For-Each loop concept :- ");
+		System.out.println("So, enter the length for your array :- ");
+		int len = ip.nextInt();
+		int [] arr = new int[len];
+		for(int i = 0; i < arr.length; i++){
+			System.out.println("Enter the element for index-no." + i + " : ");
+			arr[i] = ip.nextInt();
+		}
+		
+		System.out.println("So, your entered array is as follows :- ");
+		System.out.print("[");
+		for(int elem:arr){
+			System.out.print(elem + " ");
+			
+		}
+		System.out.print("]");
+	}
+
+}
