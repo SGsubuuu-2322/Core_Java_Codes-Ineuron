a45c316ee5ad7a9572a5b7d01df6960857c940a7
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-01-03 01:01:15 +0530

    Practice Codes

--- src/Gusser_Game/Umpire.java
+++ src/Gusser_Game/Umpire.java
@@ -7,32 +7,38 @@ public class Umpire {
 		Scanner ip = new Scanner(System.in);
 		System.out.println("This is a small guesser game :- ");
 		System.out.println("In this game a guesser will guess a number and then multiple players have to guess that number to win...");
-		Guesser gr = new Guesser();
-		gr.guessNUM();
-		System.out.println("First of all you'hv to say the number of players :- ");
-		int len = ip.nextInt();
-		Player[] pl = new Player[len];
-		
-		
-		System.out.println("Now, you all have to guess your individual number :- ");
-		for(int i = 0; i < pl.length; i++){
-			pl[i] = new Player();
-			System.out.println("Player-" + (i+1) + " guess your number :- ");
-			int num = ip.nextInt();
-			pl[i].setNum(num);
-		}
-		
-		for(int i = 0; i < pl.length; i++){
-			int plNum = pl[i].getNum();
-			int grNum = gr.getNum();
-			if(plNum == grNum){
-				System.out.println("So, player-" + i + " wins...");
+		System.out.println("So, to start and to continue this game press - '1' and to stop this press -  '-1' :- ");
+		int choice = ip.nextInt();
+		while(choice == 1){
+			Guesser gr = new Guesser();
+			gr.guessNUM();
+			System.out.println("First of all you'hv to say the number of players :- ");
+			int len = ip.nextInt();
+			Player[] pl = new Player[len];
+			
+			
+			System.out.println("Now, you all have to guess your individual number :- ");
+			for(int i = 0; i < pl.length; i++){
+				pl[i] = new Player();
+				System.out.println("Player-" + (i+1) + " guess your number :- ");
+				int num = ip.nextInt();
+				pl[i].setNum(num);
 			}
-			else{
-				System.out.println("So, player-" + i + " loses...");
+			
+			for(int i = 0; i < pl.length; i++){
+				int plNum = pl[i].getNum();
+				int grNum = gr.getNum();
+				if(plNum == grNum){
+					System.out.println("So, player-" + i + " wins...");
+				}
+				else{
+					System.out.println("So, player-" + i + " loses...");
+				}
 			}
+			System.out.println("So, to play again this game press - '1' and to stop this press - '-1' :- ");
+			choice = ip.nextInt();
 		}
-
+		System.out.println("Thanks for playing me and waiting for the next time...");
 	}
 
 }
