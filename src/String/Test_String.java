446792ed8ba66c08a06b0240a44eb71e5b99d972
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-01-06 20:49:06 +0530

    First Trial Commit from Eclipse

---
+++ src/String/Test_String.java
@@ -0,0 +1,15 @@
+package String;
+
+public class Test_String {
+
+	public static void main(String[] args) {
+		String name = "SGsubuu";
+		int count = name.toUpperCase().length();
+		System.out.println("Name is about:- " + count + " characters");
+		
+		int[] arr = {12,23,43,53,4,65,67};
+		for(int elem:arr)
+			System.out.println(elem);
+	}
+
+}
