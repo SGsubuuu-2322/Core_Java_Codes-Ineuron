167e367918586846838b24db81a1343ea4c1eb47
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-01-21 00:07:00 +0530

    Practice Codesss....

---
+++ src/PrintStream/PS_1.java
@@ -0,0 +1,16 @@
+package PrintStream;
+
+import java.io.PrintStream;
+
+public class PS_1{
+
+	public static void main(String[] args) {
+		
+		
+		PrintStream pr = System.out;
+		
+		pr.println("Hello Guysss....");
+
+	}
+
+}
