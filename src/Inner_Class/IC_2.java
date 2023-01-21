2e05800cfb83236f7fd9745e294050f20237939c
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-01-21 11:58:10 +0530

    Practice Codess....

---
+++ src/Inner_Class/IC_2.java
@@ -0,0 +1,30 @@
+package Inner_Class;
+
+
+class A{
+	
+	public void shoW(){
+		System.out.println("In Show in parent or outer class...");
+	}
+	
+	static class B{
+		public void conFig(){
+			System.out.println("In Config in inner or child class...");
+		}
+	}
+	
+}
+
+public class IC_2 {
+
+	public static void main(String[] args) {
+		
+		A a = new A();
+		a.shoW();
+		
+		A.B ab = new A.B();
+		ab.conFig();
+
+	}
+
+}
