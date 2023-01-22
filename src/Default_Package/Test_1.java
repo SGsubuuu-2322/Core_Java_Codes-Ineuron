b86ed26894872cb461b217590c863a5f8e1e698a
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-01-22 23:46:39 +0530

    Practice Codessss....

---
+++ src/Default_Package/Test_1.java
@@ -0,0 +1,16 @@
+package Default_Package;
+
+import java.io.PrintStream;
+
+public class Test_1 {
+
+	
+
+	public static void main(String[] args) {
+		PrintStream ps = System.out;
+		
+		ps.println("Helloooooo Bachooooo");
+
+	}
+
+}
