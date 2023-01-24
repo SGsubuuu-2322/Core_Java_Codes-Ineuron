88c1248df31638bbe7c14cfdfaff4d2e0e74705c
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-01-24 09:37:27 +0530

    Practice Codesss....

--- src/Interface/IF_3.java
+++ src/Interface/IF_3.java
@@ -15,10 +15,32 @@ interface I_Int4 extends I_Int1, I_Int2, I_Int3{
 	void playDis4();
 }
 
-public class IF_3 {
+class Int4 implements I_Int4{
+	public void playDis1(){
+		System.out.println("This is playDis1 method...");
+	}
+	
+	public void playDis2(){
+		System.out.println("This is playDis2 method...");
+	}
+	
+	public void playDis3(){
+		System.out.println("This is playDis3 method...");
+	}
+	
+	public void playDis4(){
+		System.out.println("This is playDis4 method...");
+	}
+}
+
+public class IF_3{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Int4 it4 = new Int4();
+		it4.playDis1();
+		it4.playDis2();
+		it4.playDis3();
+		it4.playDis4();
 
 	}
 
