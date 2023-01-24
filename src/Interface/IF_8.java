6f081c7821f49f127739eb89a93882db2a107ee3
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-01-24 21:19:09 +0530

    Practice Codessss...

---
+++ src/Interface/IF_8.java
@@ -0,0 +1,34 @@
+package Interface;
+
+interface ISITF1{
+	int metHod_1();
+}
+
+interface ISITF2{
+	void metHod_1();
+}
+
+
+class SITFImpl implements ISITF1, ISITF2{
+	
+//	public int metHod_1(){
+//		System.out.println("This is the 1st method from 1st interface....");
+//	}
+//	
+//	public void metHod_1(){
+//		System.out.println("This is the 2nd method from 2nd interface...");
+//	}
+	
+}
+
+
+public class IF_8 {
+
+	public static void main(String[] args) {
+		SITFImpl sitf = new SITFImpl();
+//		sitf.metHod_1();
+//		sitf.metHod_1();
+
+	}
+
+}
