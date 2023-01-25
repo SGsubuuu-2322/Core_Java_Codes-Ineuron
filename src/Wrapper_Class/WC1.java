25718005d3b684e516c667228b17af751a0625a5
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-01-26 00:19:33 +0530

    Practice Codesss...

---
+++ src/Wrapper_Class/WC1.java
@@ -0,0 +1,14 @@
+package Wrapper_Class;
+
+public class WC1 {
+
+	public static void main(String[] args) {
+		Integer in = new Integer("10");
+		System.out.println(in);
+		
+		Integer in1 = Integer.valueOf(21);
+		System.out.println(in1);
+
+	}
+
+}
