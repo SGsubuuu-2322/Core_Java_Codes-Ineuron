3df2f6d20a9908a3432dc93b2d3965f8e7cede8a
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-01-26 09:49:18 +0530

    Practice Codesss...

--- src/Wrapper_Class/WC2.java
+++ src/Wrapper_Class/WC2.java
@@ -8,11 +8,11 @@ public class WC2 {
 		System.out.println("MAX_RADIX = " + Character.MAX_RADIX);
 		System.out.println("MIN_RADIX = " + Character.MIN_RADIX);
 		
-		in1 = Integer.valueOf("010", 2);
+		in1 = Integer.valueOf("010", 8);
 		System.out.println(in1);
 		
 		
-		in1 = Integer.valueOf("001", 2);
+		in1 = Integer.valueOf("001", 16);
 		System.out.println(in1);
 	}
 
---
+++ src/Wrapper_Class/WC3.java
@@ -0,0 +1,16 @@
+package Wrapper_Class;
+
+public class WC3 {
+
+	public static void main(String[] args) {
+		Integer i = new Integer(130);
+		System.out.println(i.byteValue());
+		System.out.println(i.shortValue());
+		System.out.println(i.intValue());
+		System.out.println(i.longValue());
+		System.out.println(i.floatValue());
+		System.out.println(i.doubleValue());
+
+	}
+
+}
