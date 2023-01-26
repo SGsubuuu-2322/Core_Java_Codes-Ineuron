c2c5f2121b29cad175afab1a9f6da90bf87488f1
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-01-26 11:19:34 +0530

    Practice Codesss....

--- src/Wrapper_Class/WC5.java
+++ src/Wrapper_Class/WC5.java
@@ -15,6 +15,25 @@ public class WC5 {
 		
 		String s3 = Character.toString('g');
 		System.out.println(s3);
+		
+		System.out.println();
+		
+		String s4 = Integer.toString(23,2);
+		String s5 = Integer.toString(22,8);
+		String s6 = Integer.toString(14,16);
+		System.out.println(s4);
+		System.out.println(s5);
+		System.out.println(s6);
+		
+		System.out.println();
+		
+		String s7 = Integer.toBinaryString(23);
+		String s8 = Integer.toOctalString(22);
+		String s9 = Integer.toHexString(14);
+		System.out.println(s7);
+		System.out.println(s8);
+		System.out.println(s9);
+		
 
 	}
 
