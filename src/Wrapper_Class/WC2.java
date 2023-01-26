cfcd99fc76e2ed96486f632b02d34ab4e0f5cd8b
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-01-26 09:29:43 +0530

    Practice Codesss...

---
+++ src/Wrapper_Class/WC2.java
@@ -0,0 +1,19 @@
+package Wrapper_Class;
+
+public class WC2 {
+
+	public static void main(String[] args) {
+		Integer in1 = Integer.valueOf("100", 2);
+		System.out.println(in1);
+		System.out.println("MAX_RADIX = " + Character.MAX_RADIX);
+		System.out.println("MIN_RADIX = " + Character.MIN_RADIX);
+		
+		in1 = Integer.valueOf("010", 2);
+		System.out.println(in1);
+		
+		
+		in1 = Integer.valueOf("001", 2);
+		System.out.println(in1);
+	}
+
+}
