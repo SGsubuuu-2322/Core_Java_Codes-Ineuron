6f8fd5170f656acde2c9ce6a937a791e069b2afb
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-01-26 11:10:05 +0530

    Practice Codessss...

--- src/Wrapper_Class/WC5.java
+++ src/Wrapper_Class/WC5.java
@@ -3,6 +3,10 @@ package Wrapper_Class;
 public class WC5 {
 
 	public static void main(String[] args) {
+		
+		Integer i = Integer.valueOf(2322);
+		System.out.println(i);
+		
 		String s1 = Integer.toString(100);
 		System.out.println(s1);
 		
