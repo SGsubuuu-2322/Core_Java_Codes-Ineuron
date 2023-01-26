0a5afaa4c4138b96f27b0cc86bedc804b1473b69
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-01-26 11:06:55 +0530

    Practice Codesss...

---
+++ src/Wrapper_Class/WC5.java
@@ -0,0 +1,17 @@
+package Wrapper_Class;
+
+public class WC5 {
+
+	public static void main(String[] args) {
+		String s1 = Integer.toString(100);
+		System.out.println(s1);
+		
+		String s2 = Boolean.toString(false);
+		System.out.println(s2);
+		
+		String s3 = Character.toString('g');
+		System.out.println(s3);
+
+	}
+
+}
