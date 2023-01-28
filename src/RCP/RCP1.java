3e33d0e872572bf92eb952330edffb4932950874
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-01-28 20:34:08 +0530

    Practice Codesss..

--- src/RCP/RCP1.java
+++ src/RCP/RCP1.java
@@ -1,19 +1,25 @@
 package RCP;
 
-
 class Student{
+
 	static {
 		System.out.println("static block is running after class loading...");
+	}
+	
+	public Student(){
+		System.out.println("Student class constructor is called...");
 	}
 	
 	public void metHod(){
 		System.out.println("fsdgf");
 	}
+	
+	
 }
 
 public class RCP1 {
 
-	public static void main(String[] args)throws Exception {
+	public static void main(String[] args)throws ClassNotFoundException {
 		
 		String className = args[0];
 		Class c = Class.forName(className);
