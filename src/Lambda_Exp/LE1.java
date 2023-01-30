5569d903351cc21dd48385d890f697f43d907117
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-01-30 11:34:38 +0530

    Practice Codesss....

---
+++ src/Lambda_Exp/LE1.java
@@ -0,0 +1,20 @@
+package Lambda_Exp;
+
+interface SampleI{
+	void diSp();
+}
+
+public class LE1 {
+
+	public static void main(String[] args) {
+		
+		
+		SampleI si = ()-> System.out.println("Hello All Java Learnersss...");
+		
+		si.diSp();
+		
+		
+
+	}
+
+}
---
+++ src/Lambda_Exp/LE2.java
@@ -0,0 +1,20 @@
+package Lambda_Exp;
+
+interface SamplI{
+	void disPlay();
+}
+
+public class LE2 {
+
+	public static void main(String[] args) {
+		
+		SamplI sa = ()->
+		{
+			System.out.println("Hello All Java Geeks....");
+		};
+		
+		sa.disPlay();
+
+	}
+
+}
