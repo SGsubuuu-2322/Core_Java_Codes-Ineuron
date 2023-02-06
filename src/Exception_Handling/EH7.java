8ee50a4a1a4bed5c3452b8f39c2f7a3f8a5e0317
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-02-06 19:30:15 +0530

    Practice Codesss...

---
+++ src/Exception_Handling/EH7.java
@@ -0,0 +1,12 @@
+package Exception_Handling;
+
+public class EH7 {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("Before Sleepn");
+		Thread.sleep(5000);
+		System.out.println("After taking nap...");
+
+	}
+
+}
