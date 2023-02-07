dee9b4dcde136cde259bfc978ac239c9818788ec
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-02-07 12:02:40 +0530

    Practice Codesss...

--- src/Exception_Handling/Eh16.java
+++ src/Exception_Handling/Eh16.java
@@ -1,9 +1,88 @@
 package Exception_Handling;
 
+import java.util.Scanner;
+
+class DataInvalidException extends Exception{
+	public DataInvalidException(String msg) {
+		super(msg);
+	}
+}
+
+class Atm{
+	
+	Integer customerId = 232214;
+	Integer accountPwd = 2322;
+	
+	Integer cId;
+	Integer aPwd;
+	
+	public void dataInput(){
+		Scanner ip = new Scanner(System.in);
+		
+		System.out.println("Dear Sir/Madam please enter your customer-Id : ");
+		cId = ip.nextInt();
+		
+		System.out.println();
+		
+		System.out.println("Now, enter your password for your account : ");
+		aPwd = ip.nextInt();
+	}
+	
+	public void dataVerify() throws DataInvalidException
+	{
+		if((customerId == cId) && (accountPwd == aPwd)) {
+			System.out.println("Now, you can access your account...");
+		}else {
+			
+			DataInvalidException die = new DataInvalidException("Your entered data is Incorrect, So please try again...");
+			System.out.println(die.getMessage());
+			
+			throw die;
+			
+		}
+	}
+	
+}
+
+
+
+class Bank{
+	
+	public void initiate(){
+		
+		Atm a = new Atm();
+		
+		try {
+			a.dataInput();
+			a.dataVerify();
+		}catch(DataInvalidException die){
+			
+			try{
+				a.dataInput();
+				a.dataVerify();
+			}catch(DataInvalidException diee) {
+				
+				try{
+					a.dataInput();
+					a.dataVerify();
+				}catch(DataInvalidException dieee){
+					System.out.println("Aree Chor Dude!!! We'hv caught you and the card has been blocked...");
+					System.exit(0);
+				}
+				
+			}
+			
+		}
+		
+	}
+	
+}
+
 public class Eh16 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Bank b = new Bank();
+		b.initiate();
 
 	}
 
