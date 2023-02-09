7a82546b030ff06d920b8aae0d2d744452bb1278
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-02-09 12:51:05 +0530

    Practice Codesss...

--- src/MultiThreading/MT4.java
+++ src/MultiThreading/MT4.java
@@ -1,11 +1,11 @@
-//import java.sql.*;
 package MultiThreading;
+//import java.sql.*;
 
 class MyyThread extends Thread{
 	@Override 
 	public void run() {
-		System.out.println(Thread.currentThread().getPriority());
-//		Thread.currentThread().setPriority(9);
+		
+		System.out.println("Priority of the Child Thread :: " + Thread.currentThread().getPriority());
 //		System.out.println(Thread.currentThread().getPriority());
 	}
 }
@@ -15,9 +15,10 @@ public class MT4 {
 	public static void main(String[] args) {
 		
 		MyyThread myt = new MyyThread();
+		myt.setPriority(10);
 		myt.start();
 		
-		System.out.println(Thread.currentThread().getPriority());
+		System.out.println("Priority of the Main Thread :: " + Thread.currentThread().getPriority());
 
 	}
 
