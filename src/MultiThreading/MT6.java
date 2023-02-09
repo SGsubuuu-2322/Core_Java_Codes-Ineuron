324377ca105dd88f9b56aa20a3a2cc0b1ac426e5
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-02-09 21:24:52 +0530

    practice codesss....

---
+++ src/MultiThreading/MT6.java
@@ -0,0 +1,32 @@
+package MultiThreading;
+
+class ThreadClass extends Thread{
+	@Override
+	public void run() {
+		for(int i = 0; i < 6; i++){
+			System.out.println("Sita Thread...");
+			try {
+				Thread.sleep(2000);
+			}catch(InterruptedException ie) {
+				
+			}
+		}
+	}
+}
+
+public class MT6 {
+
+	public static void main(String[] args)throws InterruptedException {
+		
+		ThreadClass tc = new ThreadClass();
+		tc.start();
+		
+		tc.join(1000);
+		
+		for(int i = 0; i < 6; i++) {
+			System.out.println("Rama Thread...");
+		}
+
+	}
+
+}
