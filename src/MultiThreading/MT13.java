ae0c4b9d2157b4b5e407bda12dc93c93ef82c4c1
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-02-10 22:32:50 +0530

    Practice Codesss...

---
+++ src/MultiThreading/MT13.java
@@ -0,0 +1,23 @@
+package MultiThreading;
+
+public class MT13 {
+
+	public static void main(String[] args) {
+		
+		Thread t = new Thread(
+				new Runnable(){
+					@Override
+					public void run(){
+						for(int i = 0; i < 5; i++)
+							System.out.println("Hello I'm Child Thread...");
+					}
+				}
+				);
+		
+		t.start();
+		
+		System.out.println("Hello I'm Main Thread...");
+
+	}
+
+}
