53a0e15c5652ca39460a1d3664175d7f9ac523bd
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-02-10 09:18:28 +0530

    Practice Codesss...

--- src/MultiThreading/MT12.java
+++ src/MultiThreading/MT12.java
@@ -4,6 +4,18 @@ public class MT12 {
 
 	public static void main(String[] args) {
 		
+		Runnable r = new Runnable() {
+			@Override 
+			public void run() {
+				for(int i = 0; i < 5; i++)
+					System.out.println("This is Child Thread...");
+			}
+		};
+		
+		Thread t = new Thread(r);
+		t.start();
+		
+		System.out.println("IN the main Thread...");
 
 	}
 
