9797aee6f6f1f7412ca48b3a573bb06fda1cbd36
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-02-14 23:13:31 +0530

    Practice Code....

---
+++ src/Default_Package/PrintlnMethod.java
@@ -0,0 +1,13 @@
+package Default_Package;
+import java.io.PrintStream;
+
+public class PrintlnMethod {
+
+	public static void main(String[] args) {
+		PrintStream ps = System.out;
+		
+		ps.println("Hello EveryOne there!!! I'm SGsubuuu welcomes all to our this auspicious event...");
+
+	}
+
+}
