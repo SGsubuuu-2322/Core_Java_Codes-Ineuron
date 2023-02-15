194ffcaa91b504e5070bda23b1a1d24b442de643
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-02-15 13:14:33 +0530

    Practice code...

---
+++ src/MultiThreading/MT20.java
@@ -0,0 +1,18 @@
+package MultiThreading;
+
+public class MT20 {
+
+	public static void main(String[] args) {
+		Integer x = 2;
+		Synchronized(x){
+			System.out.println(x);
+		}
+
+	}
+
+	private static void Synchronized(Integer x) {
+		// TODO Auto-generated 
+		
+	}
+
+}
