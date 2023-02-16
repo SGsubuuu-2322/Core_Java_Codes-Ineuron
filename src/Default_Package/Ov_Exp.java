009339d8d3b86b127acf7b28bd6d82c370988b04
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-02-16 21:36:38 +0530

    Practice Codesss...

---
+++ src/Default_Package/Ov_Exp.java
@@ -0,0 +1,21 @@
+package Default_Package;
+
+class Xyz{
+	public void xyz() throws ClassNotFoundException{
+		
+	}
+}
+
+public class Ov_Exp extends Xyz{
+	
+	@Override
+	public void xyz() throws ClassNotFoundException {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
