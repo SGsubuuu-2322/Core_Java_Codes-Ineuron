141f36311b49104f46cb97c80cde6756c8a502a5
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-02-16 23:47:08 +0530

    Practice Codesss...

--- src/MultiThreading/MT20.java
+++ src/MultiThreading/MT20.java
@@ -1,18 +1,56 @@
 package MultiThreading;
 
+class DisPlay1122{
+	public void wishDish(String name){
+		for(int i = 0; i < 5; i++){
+			System.out.println("SGsubuuu...");
+		}
+		
+		synchronized(this){
+			for(int i = 0; i < 5; i++){
+				System.out.println(name + ":):):)");
+				try {
+					Thread.sleep(2000);
+				}catch(InterruptedException ie) {
+					System.out.println("Hello I got Interrupted...");
+				}
+			}
+		}
+		
+		for(int i = 0; i < 5; i++){
+			System.out.println("SGshreyansss...");
+		}
+	}
+}
+
+class ThreadClass extends Thread{
+	
+	DisPlay1122 dw;
+	String name;
+	
+	public ThreadClass(DisPlay1122 dw, String name){
+		this.dw = dw;
+		this.name = name;
+	}
+	
+	@Override 
+	public void run(){
+		dw.wishDish(name);
+	}
+}
+
 public class MT20 {
 
 	public static void main(String[] args) {
-		Integer x = 2;
-		Synchronized(x){
-			System.out.println(x);
-		}
-
-	}
-
-	private static void Synchronized(Integer x) {
-		// TODO Auto-generated 
+		DisPlay1122 dp = new DisPlay1122();
 		
+		ThreadClass t1 = new ThreadClass(dp, "GSgituuu");
+		ThreadClass t2 = new ThreadClass(dp, "SGsubuuu");
+		
+		t1.start();
+		t2.start();
+		
+		System.out.println("Hello I'm Main method...");
 	}
 
 }
