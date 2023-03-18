855e1cb599b647b4fff8dc98c9d7a1d5564d4a68
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-03-19 00:42:43 +0530

    Practice Code

--- src/Default_Package/MT6.java
+++ src/Default_Package/MT6.java
@@ -1,9 +1,26 @@
 package Default_Package;
 
+class Join extends Thread{
+	
+	@Override 
+	public void run(){
+		for(int i = 0; i < 5; i++)
+		{
+			System.out.println("Hello SGsubuuu...");
+		}
+	}
+}
+
 public class MT6 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
+		Join jn = new Join();
+		jn.start();
+		
+		for(int i = 0; i < 5; i++){
+			jn.join();
+			System.out.println("GSgituuu:):):)");
+		}
 
 	}
 
