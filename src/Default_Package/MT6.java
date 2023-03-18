8d13cf0753fab003310473f5c803ac13caa8dd9b
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-03-19 00:49:43 +0530

    Practice Codesss...

--- src/Default_Package/MT6.java
+++ src/Default_Package/MT6.java
@@ -7,6 +7,11 @@ class Join extends Thread{
 		for(int i = 0; i < 5; i++)
 		{
 			System.out.println("Hello SGsubuuu...");
+			try {
+				Thread.sleep(1000);
+			}catch(InterruptedException ie){
+				System.out.println("OOPsss!!!! I'got interrupted...");
+			}
 		}
 	}
 }
@@ -18,7 +23,7 @@ public class MT6 {
 		jn.start();
 		
 		for(int i = 0; i < 5; i++){
-			jn.join();
+			jn.join(2000);
 			System.out.println("GSgituuu:):):)");
 		}
 
